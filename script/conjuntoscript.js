f1b44c8903cbd8651a3080929979ac321e1e96d9
armandocardosoneto <armandocardos@gmail.com>
2023-09-12 01:26:44 +0000

    commit treze

---
+++ script/conjuntoscript.js
@@ -0,0 +1,10 @@
+//strings = ""/''
+//number = 1,2,3
+//boolean - true ou false
+//array = ['strings','string']
+//object = {nome: ''}
+
+let cartas = ["https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=130550&type=card", "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129710&type=card", "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129465&type=card", "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129711&type=card"];
+for (let i = 0; i < cartas.length; i = i++) {
+    console.log(cartas[i])
+}

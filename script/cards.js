ed6448e7ec5ca141b8b39fd6090724445e4c02f5
armandocardosoneto <armandocardos@gmail.com>
2023-09-20 16:36:12 +0000

    commit dezessete

--- script/cards.js
+++ script/cards.js
@@ -6,9 +6,18 @@ const card2 = "../images/card-2.jpeg";
 const card3 = "../images/card-3.jpeg";
 // const card4 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
 const card4 = "../images/card-4.jpeg";
+// const card5 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
+const card5 = "../images/card-5.jpeg";
+// const card6 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
+const card6 = "../images/card-6.jpeg";
+// const card7 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
+const card7 = "../images/card-7.jpeg";
+// const card8 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
+const card8 = "../images/card-8.jpeg";
+
 
 const image = document.querySelector(".mainclass-image img");
-const cards = [card1, card2, card3, card4];
+const cards = [card1, card2, card3, card4, card5, card6, card7, card8];
 const buttons = document.querySelectorAll(".mainclass-buttons button");
 
 buttons.forEach(function(btn, idx) {

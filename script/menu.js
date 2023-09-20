df052f0cd42604a632b2e1896ce851388f841e48
armandocardosoneto <armandocardos@gmail.com>
2023-09-20 15:26:15 +0000

    commit dezesseis

--- script/menu.js
+++ script/menu.js
@@ -3,18 +3,18 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 console.log(nav, hamburgerMenu);
 
 window.addEventListener("scroll", function() {
-   const heightScroll = window.scrollY;
+    const heightScroll = window.scrollY;
 
-   if(heightScroll > 100) {
-       nav.classList.add("change-bg");
-   }
-   else {
-       nav.classList.remove("change-bg");
-   }
+    if(heightScroll > 100) {
+        nav.classList.add("change-bg");
+    }
+    else {
+        nav.classList.remove("change-bg");
+    }
 });
 
 hamburgerMenu.addEventListener("click", (e) => {
-   e.currentTarget.classList.toggle("change-menu");
+    e.currentTarget.classList.toggle("change-menu");
 
-   nav.classList.toggle("change-nav");
+    nav.classList.toggle("change-nav");
 });

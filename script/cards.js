6d860afb404a39d8329b1e10819a4a4555a275bb
armandocardosoneto <armandocardos@gmail.com>
2023-10-08 14:07:13 +0000

    vigésimo segundo commit

--- script/cards.js
+++ script/cards.js
@@ -1,29 +1,57 @@
-// const card1 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129465&type=card";
-const card1 = "../images/card-1.jpeg";
-// const card2 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129711&type=card";
-const card2 = "../images/card-2.jpeg";
-// const card3 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129710&type=card";
-const card3 = "../images/card-3.jpeg";
-// const card4 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card4 = "../images/card-4.jpeg";
-// const card5 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card5 = "../images/card-5.jpeg";
-// const card6 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card6 = "../images/card-6.jpeg";
-// const card7 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card7 = "../images/card-7.jpeg";
-// const card8 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149551&type=card";
-const card8 = "../images/card-8.jpeg";
-
-
 const image = document.querySelector(".mainclass-image img");
-const cards = [card1, card2, card3, card4, card5, card6, card7, card8];
 const buttons = document.querySelectorAll(".mainclass-buttons button");
+const apiUrl = "https://api.magicthegathering.io/v1/";
 
-buttons.forEach(function (btn, idx) {
-    btn.addEventListener("click", function () {
-        console.log(btn);
-
-        image.src = cards[idx];
+buttons.forEach(function(btn, idx) {
+    btn.addEventListener("click", function() {
+        getCards(idx);
     });
 });
+
+async function getCards(index) {
+    const path = "cards";
+    const endPoint = apiUrl + path;
+
+    try {
+        const response = await fetch(endPoint, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        const result = await response.json();
+        // console.log(result);
+
+        const firstCard = result.cards[2];
+        const firstCardImage = firstCard.imageUrl;        
+
+        const secondCard = result.cards[6];
+        const secondCardImage = secondCard.imageUrl;        
+
+        const thirdCard = result.cards[7];
+        const thirdCardImage = thirdCard.imageUrl;        
+
+        const fourthCard = result.cards[12];
+        const fourthCardImage = fourthCard.imageUrl;
+        
+        const fifthCard = result.cards[15];
+        const fifthCardImage = fifthCard.imageUrl;
+        
+        const sixthCard = result.cards[18];
+        const sixthCardImage = sixthCard.imageUrl;  
+
+        const seventhCard = result.cards[19];
+        const seventhCardImage = seventhCard.imageUrl;  
+
+        const eighthCard = result.cards[20];
+        const eighthCardImage = eighthCard.imageUrl;  
+
+        const arrayImages = [firstCardImage, secondCardImage, thirdCardImage, fourthCardImage, fifthCardImage, sixthCardImage, seventhCardImage, eighthCardImage];
+
+        image.src = arrayImages[index];
+    }
+    catch(error) {
+        console.error("Error", error);
+    }
+}
